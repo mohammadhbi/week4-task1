2e568cc9da0a2f8f54303377152ba43cd36c0a16
mohammad <mohammad19hobbi@gmail.com>
2025-02-12 19:03:45 +0330

    footer part 1 completed

--- newreactproject/src/App.jsx
+++ newreactproject/src/App.jsx
@@ -1,12 +1,14 @@
 import Navbar from "./component/navbar/Navbar";
 import { Header } from "./component/hedear/Header";
 import { MainSection } from "./component/mainSecton/mainSection";
+import { Footer } from "./component/footer/footer";
 function App(){
   return(
    <div>
      <Navbar/>
      <Header/>
      <MainSection />
+     <Footer/>
    </div>
   )
 }
